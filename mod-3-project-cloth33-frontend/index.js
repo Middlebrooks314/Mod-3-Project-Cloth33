31c57ffaef3cfe790c080e479f3d9a27b1588b51
github username <github email address>
2019-08-19 14:06:12 -0500

    fetch-setup for logging in, few basic functions marked for work

--- mod-3-project-cloth33-frontend/index.js
+++ mod-3-project-cloth33-frontend/index.js
@@ -1,23 +1,54 @@
+const hostURL = 'http://localhost:3000/'
+const defaultElementStyle = 'block'
 // DOMContentLoaded
+
 document.addEventListener('DOMContentLoaded' , ()=>{
     const loginForm = document.getElementById('login-form')
     loginForm.addEventListener('submit' , (e) =>{
         e.preventDefault();
         hideElement(loginForm , false)
-        loadStartPage()
+        loadClosetManager()
     })
 })
 // DOMContentLoaded End
 
 function hideElement(htmlElement , makeVisible){
     if(makeVisible)
-        htmlElement.style.display = 'block'
+        htmlElement.style.display = defaultElementStyle
     else
         htmlElement.style.display = 'none'
 }
 
 
-function loadStartPage(){
-    console.log('Going to Starting Page')
-    
+
+// load closet / clothes manager to ADD , VIEW , and , DELETE CLOTHES
+function loadClosetManager(){
+
+}
+
+//load the outfit-creation screen to USE EXISTING CLOTHES
+function loadOutfitCreator(){
+
+}
+
+// view outfits
+function outfitView(){
+
+}
+
+function loginUser(username){
+    fetch(hostURL , {
+        method: 'POST' ,
+        headers:{
+            'Content-Type' : 'application/json' , 
+            'Accept' : 'application/json'
+        } , 
+        body: JSON.stringify({
+            'username' : username
+        })
+    }).then(resp =>{
+        return resp.json();
+    }).then(json =>{
+        console.log(json)
+    })
 }
