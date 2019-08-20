c006698e4d7e9c7189451dee3483a0e66ef8af38
github username <github email address>
2019-08-19 20:39:00 -0500

    pulled master

--- mod-3-project-cloth33-backend-api/app/controllers/users_controller.rb
+++ mod-3-project-cloth33-backend-api/app/controllers/users_controller.rb
@@ -1,23 +1,19 @@
 class UsersController < ApplicationController
     
-
-
     def create
         user = User.create(user_params)
         render json: user
     end 
-        
 
     def show 
-        user = User.find_by(id: params[:id])
-        render json: user, only: [:username], include: [:items]
+        user = User.find_by(username: params[:username])
+        render json: user, include: [:items]
     end 
 
     def destroy
         User.find_by(id: params[:id]).destroy
         render json: "User Deleted!"
     end
-
 
     private 
 
