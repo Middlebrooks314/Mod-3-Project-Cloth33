f278c336f2e1ccdf047cdef1bb7d11526c32a32c
github username <github email address>
2019-08-20 15:25:42 -0500

    fixed the rendering of the items

--- mod-3-project-cloth33-frontend/index.js
+++ mod-3-project-cloth33-frontend/index.js
@@ -1,17 +1,18 @@
 const hostURL = 'http://localhost:3000/'
 const defaultElementStyle = 'flex'
 const itemForm = document.getElementById('item-form')
+const closetDiv = document.getElementById('item-container')
 // DOMContentLoaded
-// let userId = 1
+let userId = 1
 
 
 document.addEventListener('DOMContentLoaded' , ()=>{
     const loginForm = document.getElementById('login-form')
-    loginForm.addEventListener('submit' , (e) =>{
-        e.preventDefault();
+    loginForm.addEventListener('submit' , (event) =>{
+        event.preventDefault();
         loginUser(loginForm.username.value);
         hideElement(loginForm , false)
-        
+        addNewItem()
     })
 })
 
@@ -29,19 +30,32 @@ function loginUser(name){
         return resp.json();
     }).then(user=>{
         console.log(user)
-        let userId = user['id']
+        userId = user['id']
         console.log(userId)
-        renderNewItem(user.items)
+        renderNewItems(user.items)
     })
 }
 
 // load closet / clothes manager to ADD , VIEW , and , DELETE CLOTHES
-const renderNewItem = (items) => {
-    console.log(items)
-    // let itemDiv = document.createElement('div')
-    // let itemNameH3 = document.createElement('H3')
-    // let itemImage = document.createElement('img')
-        // itemImage.src = 
+const renderNewItems = (items) => {
+    // console.log(items)
+    for(const item of items){
+        createItemElements(item)    
+    }    
+}
+
+const createItemElements = (item) => {
+    let itemDiv = document.createElement('div')
+    let itemNameH3 = document.createElement('h4')
+        itemNameH3.innerText = item.name
+    let itemImage = document.createElement('img')
+        itemImage.className = 'item-avatar'
+        itemImage.src = item.img_url
+    let deleteButton = document.createElement('button')
+        deleteButton.innerHTML = 'X'
+
+    itemDiv.append(itemNameH3, itemImage, deleteButton)
+    closetDiv.append(itemDiv)
 
 }
 
@@ -64,7 +78,10 @@ function addNewItem() {
             body: JSON.stringify(formData)
         })
         .then(resp => resp.json())
-        .then(console.log)
+        .then(item => {
+            createItemElements(item)
+            console.log(item)
+        })
     })
 }
 
