267551a60d8ceca425bc2317a69d3aae81ac2131
github username <github email address>
2019-08-21 13:13:26 -0500

    built custom routes for user login

--- mod-3-project-cloth33-backend-api/app/controllers/users_controller.rb
+++ mod-3-project-cloth33-backend-api/app/controllers/users_controller.rb
@@ -2,51 +2,67 @@ class UsersController < ApplicationController
     
     def index
 
-        user = User.find_by(username: params[:username])
-        render json: user, include:[:items, :outfits]
-        # users = User.all
-        # #render json: users , include:[:items , :outfits]
-        # render json: users.to_json(include: {
-        #     items: {except: [:created_at , :updated_at]} ,
-        #     outfits: {except: [:created_at , :updated_at] , include: {
-        #         items: {except: [:created_at , :updated_at]}
-        #     }}
-        # },
-        # except: [:created_at , :updated_at])
+        # user = User.find_by(username: params[:username])
+        # render json: user, include:[:items, :outfits]
+        users = User.all
+        #render json: users , include:[:items , :outfits]
+        render json: users.to_json(include: {
+            items: {except: [:created_at , :updated_at]} ,
+            outfits: {except: [:created_at , :updated_at] , include: {
+                items: {except: [:created_at , :updated_at]}
+            }}
+        },
+        except: [:created_at , :updated_at])
     end
 
-    def create
-        user = User.create(user_params)
-        # random-generated clothing
-        30.times do |new_item|
-            new_url = "https://picsum.photos/id/#{rand(1..1000)}/200/300"
-
-            new_item = Item.new(user_id: user.id , category: Item.categories.shuffle[0] , img_url: new_url , name: 'Name!')
-            new_item.name = "#{Faker::Company.name} #{Item.clothing_types.shuffle[0]}"
-            new_item.save
-        end
-
-        6.times do
-            outfit = Outfit.new(user_id: user.id)
-            temp = []
-            3.times do
-                new_item = user.items.shuffle[0]
-                temp << new_item
-            end
-            puts '====================================='
-            puts temp
-            temp = temp.uniq
-            temp.each do |item|
-                outfit.items << item
-            end
-
-            outfit.save
-        end
-
+    def login
+        user = User.find_by(username: params[:username])
+        
+        # user = User.create(user_params)
+  
         render json: user.to_json(include: {
             items: {except: [:created_at , :updated_at]} ,
             outfits: {except: [:created_at , :updated_at] , include: {
                 items: {except: [:created_at , :updated_at]}
+            }}
+        },
+        except: [:created_at , :updated_at])
+    end 
+
+
+    def create
+        user = User.create(user_params)
+        # random-generated clothing
+        # 30.times do |new_item|
+        #     new_url = "https://picsum.photos/id/#{rand(1..1000)}/200/300"
+
+        #     new_item = Item.new(user_id: user.id , category: Item.categories.shuffle[0] , img_url: new_url , name: 'Name!')
+        #     new_item.name = "#{Faker::Company.name} #{Item.clothing_types.shuffle[0]}"
+        #     new_item.save
+        # end
+
+        # 6.times do
+        #     outfit = Outfit.new(user_id: user.id)
+        #     temp = []
+        #     3.times do
+        #         new_item = user.items.shuffle[0]
+        #         temp << new_item
+        #     end
+        #     puts '====================================='
+        #     puts temp
+        #     temp = temp.uniq
+        #     temp.each do |item|
+        #         outfit.items << item
+        #     end
+
+        #     outfit.save
+        # end
+
+        render json: user
+        # .to_json(include: {
+            # items: {except: [:created_at , :updated_at]} ,
+            # outfits: {except: [:created_at , :updated_at] , include: {
+                # items: {except: [:created_at , :updated_at]}
             }}
         },
         except: [:created_at , :updated_at])
