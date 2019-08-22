4f8c4bf3d69a804d3875747f9eed96ec083eaa67
github username <github email address>
2019-08-22 10:00:04 -0500

    validations and seeds

--- mod-3-project-cloth33-backend-api/app/models/user.rb
+++ mod-3-project-cloth33-backend-api/app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :outfits, dependent: :destroy
     has_many :items, dependent: :destroy
     validates :username, presence: true, uniqueness: true
-    validates_length_of :items, maximum: 33
+    validates_length_of :items, maximum: 32
 end
